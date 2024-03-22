60b0fb60c03d64cd4b7e845bc39136e6913e5282
Artem Baredov <artbar77777@gmail.com>
2024-03-22 23:46:32 +0300

    home work #5

---
+++ lesson5/src/main/java/ru/geekbrains/oop/lesson5/presenters/ViewObserver.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.oop.lesson5.presenters;
+
+import java.util.Date;
+
+public interface ViewObserver {
+
+    void onReservationTable(Date orderDate, int tableNo, String name);
+
+    void onСhangeReservationTable(int oldReservation, Date reservationDate, int tableNo, String name);
+
+}
